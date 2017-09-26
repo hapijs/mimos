d1848bbebf06a649fd488211bbb8fd9c30dbe148
Eran Hammer <eran@hammer.io>
2017-09-26 01:37:32 -0700

    node 8. Closes #20

--- test/index.js
+++ test/index.js
@@ -14,16 +14,14 @@ const internals = {};
 
 // Test shortcuts
 
-const lab = exports.lab = Lab.script();
-const describe = lab.describe;
-const it = lab.it;
+const { describe, it } = exports.lab = Lab.script();
 const expect = Code.expect;
 
 describe('Mimos', () => {
 
     describe('path()', () => {
 
-        it('returns the mime type from a file path', (done) => {
+        it('returns the mime type from a file path', async () => {
 
             const mimos = new Mimos();
 
@@ -31,35 +29,31 @@ describe('Mimos', () => {
                 source: 'iana',
                 charset: 'UTF-8',
                 compressible: true,
-                extensions: ['js'],
+                extensions: ['js', 'mjs'],
                 type: 'application/javascript'
             });
-            done();
         });
 
-        it('returns empty object if a match can not be found', (done) => {
+        it('returns empty object if a match can not be found', async () => {
 
             const mimos = new Mimos();
 
             expect(mimos.path('/static/javascript')).to.equal({});
-            done();
         });
 
-        it('ignores extension upper case', (done) => {
+        it('ignores extension upper case', async () => {
 
             const lower = '/static/image/image.jpg';
             const upper = '/static/image/image.JPG';
             const mimos = new Mimos();
 
             expect(mimos.path(lower).type).to.equal(mimos.path(upper).type);
-
-            done();
         });
     });
 
     describe('type()', () => {
 
-        it('returns a found type', (done) => {
+        it('returns a found type', async () => {
 
             const mimos = new Mimos();
 
@@ -69,10 +63,9 @@ describe('Mimos', () => {
                 extensions: ['txt', 'text', 'conf', 'def', 'list', 'log', 'in', 'ini'],
                 type: 'text/plain'
             });
-            done();
         });
 
-        it('returns a missing type', (done) => {
+        it('returns a missing type', async () => {
 
             const mimos = new Mimos();
 
@@ -82,11 +75,10 @@ describe('Mimos', () => {
                 extensions: [],
                 type: 'hapi/test'
             });
-            done();
         });
     });
 
-    it('accepts an override object to make adjustments to the internal mime database', (done) => {
+    it('accepts an override object to make adjustments to the internal mime database', async () => {
 
         const nodeModule = {
             source: 'iana',
@@ -103,11 +95,9 @@ describe('Mimos', () => {
         const mimos = new Mimos(dbOverwrite);
         expect(mimos.type('node/module')).to.equal(nodeModule);
         expect(mimos.path('/node_modules/node/module.npm')).to.equal(nodeModule);
-
-        done();
     });
 
-    it('allows built-in types to be replaced with user mime data', (done) => {
+    it('allows built-in types to be replaced with user mime data', async () => {
 
         const jsModule = {
             source: 'iana',
@@ -126,11 +116,9 @@ describe('Mimos', () => {
 
         expect(mimos.type('application/javascript')).to.equal(jsModule);
         expect(mimos.path('/static/js/app.js')).to.equal(jsModule);
-
-        done();
     });
 
-    it('executes a predicate function if it is provided', (done) => {
+    it('executes a predicate function if it is provided', async () => {
 
         const jsModule = {
             predicate: function (mime) {
@@ -163,21 +151,17 @@ describe('Mimos', () => {
             foo: 'bar',
             type: 'text/javascript'
         });
-
-        done();
     });
 
-    it('throws an error if created without new', (done) => {
+    it('throws an error if created without new', async () => {
 
         expect(() => {
 
             Mimos();
         }).to.throw(/cannot be invoked without 'new'/g);
-        done();
-
     });
 
-    it('throws an error if the predicate option is not a functino', (done) => {
+    it('throws an error if the predicate option is not a functino', async () => {
 
         expect(() => {
 
@@ -189,6 +173,5 @@ describe('Mimos', () => {
                 }
             });
         }).to.throw('predicate option must be a function');
-        done();
     });
 });
