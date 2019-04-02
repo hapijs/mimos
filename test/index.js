9216552f30b4fcab2fa0f06856329ad8270344c5
Eran Hammer <eran@hammer.io>
2019-04-01 22:47:21 -0700

    Update dep. Closes #25

--- test/index.js
+++ test/index.js
@@ -1,18 +1,12 @@
 'use strict';
 
-// Load modules
-
-const Code = require('code');
-const Lab = require('lab');
+const Code = require('@hapi/code');
+const Lab = require('@hapi/lab');
 const Mimos = require('..');
 
 
-// Declare internals
-
 const internals = {};
 
-
-// Test shortcuts
 
 const { describe, it } = exports.lab = Lab.script();
 const expect = Code.expect;
