b77c029e1e5078d17474964dedf3eb5d32ec6a4a
Gil Pedersen <git@gpost.dk>
2021-04-11 00:39:21 +0200

    Add typescript types
    This should be compatible with the current usage in @types/hapi__hapi.

---
+++ test/index.ts
@@ -0,0 +1,38 @@
+import { Mimos, MimosEntry } from '..';
+import * as Lab from '@hapi/lab';
+
+const { expect } = Lab.types;
+
+
+const mimos = new Mimos();
+const predicate = (entry: MimosEntry) => entry;
+
+// constructor()
+
+expect.type<Mimos>(mimos);
+expect.type<Mimos>(new Mimos({ override: { 'test/html':  { compressible: false, predicate } }}));
+expect.type<Mimos>(new Mimos<{ custom: boolean }>({ override: { 'test/html': { compressible: false, predicate, custom: true } } }));
+
+expect.error(new Mimos({ unknown: true }));
+expect.error(new Mimos({ override: new Map() }));
+expect.error(new Mimos({ override: [{}] }));
+expect.error(new Mimos({ override: { 'test/html': { compressible: 0 } } }));
+expect.error(new Mimos({ override: { 'test/html': { predicate: () => true } } }));
+expect.error(new Mimos({ override: { 'test/html': { compressible: false, predicate, custom: true } } }));
+
+
+// Mimos.type()
+
+expect.type<MimosEntry>(mimos.type('text/html'));
+expect.type<MimosEntry & { custom?: boolean }>(new Mimos<{ custom: boolean }>().type('text/html'))
+
+expect.error(mimos.type());
+expect.error(mimos.type(true));
+
+// Mimos.path()
+
+expect.type<MimosEntry | null>(mimos.path('/my/file.html'));
+expect.type<MimosEntry & { custom?: boolean } | null>(new Mimos<{ custom: boolean }>().path('/my/file.html'))
+
+expect.error(mimos.path());
+expect.error(mimos.path(new URL('file:///my/file.html')));
