850c74586047c42992cc9ea05b43e9a7d557b8a0
devin ivy <devin@bigroomstudios.com>
2022-05-22 23:54:19 -0400

    Support node v18, drop node v12 (#41)

---
+++ test/esm.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Code = require('@hapi/code');
+const Lab = require('@hapi/lab');
+
+
+const { before, describe, it } = exports.lab = Lab.script();
+const expect = Code.expect;
+
+
+describe('import()', () => {
+
+    let Mimos;
+
+    before(async () => {
+
+        Mimos = await import('../lib/index.js');
+    });
+
+    it('exposes all methods and classes as named imports', () => {
+
+        expect(Object.keys(Mimos)).to.equal([
+            'Mimos',
+            'MimosEntry',
+            'default'
+        ]);
+    });
+});
